7f163a16c3f4154197669c5971fb527833fd1208
Yoennis Pavon Ortega <yoepo2016@gmail.com>
2024-05-30 17:34:42 -0400

    Finalizando

--- UtilesGenerales/UtilesGenerales.cs
+++ UtilesGenerales/UtilesGenerales.cs
@@ -1,7 +1,35 @@
 ﻿using Dapper;
 using System.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
 namespace UtilesGenerales
 {
+
+    public class VMDatosClientes
+    {
+        public string idcliente { get; set; }
+        public string nombrecliente { get; set; }
+        public string codigo { get; set; }
+        public string direccion { get; set; }
+        public string nombredirector { get; set; }
+        public string correoelectronico { get; set; }
+        public string telefono { get; set; }
+        public string contactocomercial { get; set; }
+
+        public string cargo { get; set; }
+        public string telefonocontacto { get; set; }
+        public string correocontacto { get; set; }
+        public string cuentacup { get; set; }
+        public string banco { get; set; }
+        public string sucursal { get; set; }
+        public string direccionbanco { get; set; }
+        public string nrocuentacl { get; set; }
+        public string titularcuentacl { get; set; }
+        public string bancocl { get; set; }
+        public string   sucursalcl { get; set; }
+        public string direccionbancocl { get; set; }
+        public string idorganismo { get; set; }
+    }
+
     public class ConexionSql
     {
         private readonly Dictionary<string, string> _cadenaConexion = new Dictionary<string, string> {
@@ -27,5 +55,12 @@ namespace UtilesGenerales
             return resultado;
 
         }
+
+        public static bool introducirNuevoCliente( VMDatosClientes cliente)
+        {
+
+        }
+
+
     }
 }
